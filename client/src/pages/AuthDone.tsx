8272f7ea358c64107aaa57015a196eafcdd9dc7c
shivdutt-B <shivdutt059@gmail.com>
2025-02-18 20:36:10 +0530

    fixes

--- client/src/pages/AuthDone.tsx
+++ client/src/pages/AuthDone.tsx
@@ -1,12 +1,45 @@
 import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function AuthDone() {
+    async function getUser() {
+
+        console.log("user data", window.location)
+
+        console.log('REACHED2')
+        const params = new URLSearchParams(window.location.search);
+        const code = params.get("code");
+        // if (!code) return res.status(400).json({ error: "Authorization code missing" });
+
+        try {
+            // Step 3: Exchange code for access token
+            const tokenResponse = await axios.post("https://github.com/login/oauth/access_token", null, {
+                params: {
+                    client_id: import.meta.env.VITE_CLIENT_ID,
+                    client_secret: import.meta.env.GITHUB_CLIENT_SECRET,
+                    code,
+                },
+                headers: { Accept: "application/json" },
+            });
+
+            const accessToken = tokenResponse.data.access_token;
+
+            // if (!accessToken) return res.status(400).json({ error: "Failed to get access token" });
+
+            // Step 4: Fetch user details
+            const userResponse = await axios.get("https://api.github.com/user", {
+                headers: { Authorization: `token ${accessToken}` },
+            });
+            console.log('USER', userResponse)
+        }
+
+    }
     useEffect(() => {
-        console.log("user data",window.location)
-    })
-  return (
-    <div>AuthDone</div>
-  )
+        getUser();
+    }, [])
+    return (
+        <div>AuthDone</div>
+    )
 }
 
 export default AuthDone
