3c8ec715d13bcb14d6361d99a47df6ece02b3e3a
shivdutt-B <shivdutt059@gmail.com>
2025-02-19 01:26:15 +0530

    fixed authdone

--- client/src/pages/AuthDone.tsx
+++ client/src/pages/AuthDone.tsx
@@ -1,60 +1,60 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { ButtonLoading } from "../components/ui/LoadingBtn";
+// import React, { useEffect } from "react";
+// import axios from "axios";
+// import { ButtonLoading } from "../components/ui/LoadingBtn";
 
-function AuthDone() {
-    async function getUser() {
-        console.log("User data:", window.location);
+// function AuthDone() {
+//     async function getUser() {
+//         console.log("User data:", window.location);
 
-        const params = new URLSearchParams(window.location.search);
-        const code = params.get("code");
+//         const params = new URLSearchParams(window.location.search);
+//         const code = params.get("code");
 
-        if (!code) {
-            console.error("Authorization code is missing");
-            return;
-        }
+//         if (!code) {
+//             console.error("Authorization code is missing");
+//             return;
+//         }
 
-        try {
-            // Exchange code for access token
-            const tokenResponse = await axios.post(
-                "https://github.com/login/oauth/access_token",
-                null,
-                {
-                    params: {
-                        client_id: import.meta.env.GITHUB_CLIENT_ID,
-                        client_secret: import.meta.env.GITHUB_CLIENT_SECRET, // Fixed env variable
-                        code,
-                    },
-                    headers: { Accept: "application/json" },
-                }
-            );
+//         try {
+//             // Exchange code for access token
+//             const tokenResponse = await axios.post(
+//                 "https://github.com/login/oauth/access_token",
+//                 null,
+//                 {
+//                     params: {
+//                         client_id: import.meta.env.GITHUB_CLIENT_ID,
+//                         client_secret: import.meta.env.GITHUB_CLIENT_SECRET, // Fixed env variable
+//                         code,
+//                     },
+//                     headers: { Accept: "application/json" },
+//                 }
+//             );
 
-            const accessToken = tokenResponse.data.access_token;
+//             const accessToken = tokenResponse.data.access_token;
 
-            if (!accessToken) {
-                console.error("Failed to retrieve access token");
-                return;
-            }
+//             if (!accessToken) {
+//                 console.error("Failed to retrieve access token");
+//                 return;
+//             }
 
-            // Fetch user details
-            const userResponse = await axios.get("https://api.github.com/user", {
-                headers: { Authorization: `token ${accessToken}` },
-            });
+//             // Fetch user details
+//             const userResponse = await axios.get("https://api.github.com/user", {
+//                 headers: { Authorization: `token ${accessToken}` },
+//             });
 
-            console.log("USER:", userResponse.data);
-        } catch (error) {
-            console.error("Error fetching user:", error);
-        }
-    }
+//             console.log("USER:", userResponse.data);
+//         } catch (error) {
+//             console.error("Error fetching user:", error);
+//         }
+//     }
 
-    useEffect(() => {
-        getUser();
-    }, []);
+//     useEffect(() => {
+//         getUser();
+//     }, []);
 
-    return <div>AuthDone</div>;
-}
+//     return <div>AuthDone</div>;
+// }
 
-export default AuthDone;
+// export default AuthDone;
 
 
 
