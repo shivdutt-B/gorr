305c2be36a26c40ca08be1e2d5166d4f99fdc347
shivdutt-B <shivdutt059@gmail.com>
2025-04-06 23:53:43 +0530

    project name after main url

--- proxy-server/main.js
+++ proxy-server/main.js
@@ -2,34 +2,35 @@ const express = require("express");
 const httpProxy = require("http-proxy");
 require("dotenv").config();
 
-// Initialize Express and set up the proxy
 const app = express();
 const PORT = process.env.PORT || 8000;
-// const BASE_PATH = process.env.S3_BASE_PATH;
 const BASE_PATH = process.env.S3_BASE_PATH;
 const proxy = httpProxy.createProxy();
 
-// Middleware to handle incoming requests and proxy them to the correct S3 path
-app.use((req, res) => {
-  const hostname = req.hostname;
-  const subdomain = hostname.split(".").slice(0, -2).join("/");
+// Middleware to handle path-based proxying
+app.use("/*", (req, res) => {
+  const pathFromUrl = req.path.substring(1); // remove leading slash
 
-  // Constructing the target URL for the proxy based on the subdomain
-  const resolvesTo = `${BASE_PATH}/${subdomain}`;
+  pathFromUrl = pathFromUrl.replaceAll(".", "/");
 
-  // Proxying the request to the constructed URL
-  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+  console.log("pathFromUrl: ", pathFromUrl);
+
+  // This constructs: http://s3-bucket-url/my-ang-app/angular-fresh/browser
+  const resolvesTo = `${BASE_PATH}/${pathFromUrl}`;
+
+  // Forward the request to S3
+  proxy.web(req, res, {
+    target: resolvesTo,
+    changeOrigin: true,
+    ignorePath: true, // important to avoid appending original path to the end again
+  });
 });
 
-// Event listener for proxy requests to modify the path if necessary
-// Although this is handled in the AWS S3 bucket itself, we are doing it here for the sake of completeness.
-proxy.on("proxyReq", (proxyReq, req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    // Appending 'index.html' to the path if the URL is the root
+// Automatically serve index.html if root is requested
+proxy.on("proxyReq", (proxyReq, req) => {
+  if (req.url === "/" || req.url.endsWith("/")) {
     proxyReq.path += "index.html";
   }
 });
 
-// Starting the server and logging the port number
 app.listen(PORT, () => console.log(`Reverse Proxy Running on port ${PORT}`));
