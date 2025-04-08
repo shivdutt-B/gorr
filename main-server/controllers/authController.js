e67212d18d978b00c52eef6e30a136d52d53356c
shivdutt-B <shivdutt059@gmail.com>
2025-04-09 01:02:05 +0530

    same site none

--- main-server/controllers/authController.js
+++ main-server/controllers/authController.js
@@ -41,7 +41,7 @@ const githubCallback = async (req, res) => {
 
     res.cookie("github_token", accessToken, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
